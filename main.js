46d104ac5a0e71bed449782653bf30097a5db2b8
SarvarbekNematov <meoff1011@gmail.com>
2024-06-03 03:38:37 +0500

    vazifa

--- main.js
+++ main.js
@@ -1,31 +1,80 @@
 const form = document.querySelector(".form");
 const input = document.querySelector(".input");
-const send = document.querySelector(".send");
 const box = document.querySelector(".box");
+const modal = document.querySelector(".modal");
+const content = document.querySelector(".content");
+const close__btn = document.querySelector(".close__btn")
 
+let data = [];
 
-  const render = () => {
-      box.innerHTML = data.map((item) =>
-        
-        `
-          <div>
-              <h2>${item.name}</h2>
-          </div>
-      `).join("")
-  }
+const render = () => {
+    const item = data.map((item) => {
+      return `
+        <div class = "card">
+          <h1>${item.name}</h1>
+          <button onclick="deleteItem(${item.id})">delete</button>
+          <button onclick="editItem(${item.id})">edit</button>
+          <button data-id = "${item.id}" class = "open_modal">submit</button>
+        </div>
+      `;
+    });
+    box.innerHTML = item.join("");
+    const card = document.querySelector(".card")
+  };
   
-  let data = JSON.parse(localStorage.getItem("name")) || []
 
-form.addEventListener("submit", (e) => {
+  form.addEventListener("submit", (e) => {
     e.preventDefault();
-    const oldData = JSON.parse(localStorage.getItem("name") ) || [];
+  
+    data.push({ name: input.value, id: Date.now() });
+    input.value = "";
+    render();
+  });
+  
+  const deleteItem = (id) => {
+    console.log(id);
+  
+    for (let i = 0; i < data.length; i++) {
+      if (data[i].id == id) {
+        data.splice(i, 1);
+      }
+    }
+    render();
+  };
+  
+  const editItem = (id) => {
+    const item = data.find((values) => values.id == id);
+    let newTitle = prompt("", item.name);
+  
+    for (let i = 0; i < data.length; i++) {
+      if (data[i].id == id) {
+        data[i].name = newTitle;
+      }
+    }
+  
+    render();
+  };
+  
 
-    const newValue = JSON.stringify([{name : input.value , id : Date.now} , ...oldData])
-    localStorage.setItem("name" , newValue)
-    // data.push({name : input.value , id : Date.now});
+  const renderSingle = (data) => {
+    content.innerHTML = `
+    <div class = "card_two">
+        <h2>${data.name}</h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis adipisci, distinctio amet repudiandae earum doloremque explicabo?
+        </p>
+    </div>
+    `
 
-    // data.push(JSON.parse(newValue)) 
-    render()
-    input.value = ""
+}
+
+
+  card.addEventListener("click", (e) => {
+    if(e.target.dataset.id){
+        modal.classList.add("active");
+        renderSingle(data)
+    }
+  })
+
+  close__btn.addEventListener("click", () => {
+    modal.classList.remove("active")
 })
-
